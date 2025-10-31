e253b40ab70f75fed562fcb771dae4e03e5c6506
juliogo666-art <julio.go666@gmail.com>
2025-10-31 19:57:40 +0100

    Initial commit for orchestratpor

---
+++ scripts/Orquestrador.py
@@ -0,0 +1,18 @@
+# Importar las classes o objetos
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# GenerarNumeroPoblacionTotal()
+
+from clean_data import data_cleaners 
+from filter_data import data_filter
+from data_visualization import DataVisualization # OK
+from data_RnD import RnD
+
+# visualizar datos con una grafica pasamos datos X, Y
+DataVisualization.showBarGraphic()
+
+
