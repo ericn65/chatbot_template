e30b46efb1defa98553805865740c83c56f436cc
ericn65 <saericquintana@gmail.com>
2025-11-23 11:53:46 +0100

    adding stuff for the classification.

--- chatbot_template/utils/teacher/forecasting.py
+++ chatbot_template/utils/teacher/forecasting.py
@@ -1,6 +1,6 @@
 import joblib
 import pandas as pd
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 
 
 def train_regression_model(
@@ -39,6 +39,51 @@ def train_regression_model(
     y_test = labels.iloc[split:]
 
     model = RandomForestRegressor()
+    model.fit(X_train, y_train)
+
+    joblib.dump(model, model_path)
+
+    predictions = pd.Series(model.predict(X_test), index=y_test.index)
+
+    return model, predictions
+
+
+def train_classifier_model(
+    features: pd.DataFrame,
+    labels: pd.Series,
+    split_factor: float = 0.8,
+    model_path: str = "./models/trained_model.pkl",
+) -> tuple[RandomForestClassifier, pd.Series]:
+    """
+    Train a RandomForest model for forecasting classifiers and save it to disk.
+
+    Parameters
+    ----------
+    features : pd.DataFrame
+        Feature matrix (lags, rolling windows, external variables).
+    labels : pd.Series
+        Target variable.
+    split_factor : float
+        Proportion of data used for training. Default: 0.8.
+    model_path : str
+        Path to save the trained model.
+
+    Returns
+    -------
+    model : RandomForestClassifier
+        The trained model object.
+    predictions : pd.Series
+        Predictions on the test set (for evaluation).
+    """
+    split = int(len(features) * split_factor)
+
+    X_train = features.iloc[:split]
+    X_test = features.iloc[split:]
+
+    y_train = labels.iloc[:split]
+    y_test = labels.iloc[split:]
+
+    model = RandomForestClassifier()
     model.fit(X_train, y_train)
 
     joblib.dump(model, model_path)
