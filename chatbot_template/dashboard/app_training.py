30445a6f0c8d418bfae5791db117a15ecc3cc206
ericn65 <saericquintana@gmail.com>
2025-11-23 12:28:37 +0100

    Adding dashboard and running

---
+++ chatbot_template/dashboard/app_training.py
@@ -0,0 +1,20 @@
+from training_dashboard import run_training_dashboard
+
+from chatbot_template.config.config_dashboard import DashboardConfig
+
+
+def run_runing_training_dashboard(config: DashboardConfig | None = None):
+    """
+    Runs the correlation visualization dashboard.
+
+    Parameters
+    ----------
+    config : DashboardConfig
+        Dashboard configuration (title, default data path, layout…).
+    """
+    # config = config or DashboardConfig()
+    run_training_dashboard()
+
+
+if __name__ == "__main__":
+    run_runing_training_dashboard()
---
+++ scripts/run_training_dashboard.py
@@ -0,0 +1,4 @@
+from chatbot_template.dashboard.app_training import run_runing_training_dashboard
+
+if __name__ == "__main__":
+    run_runing_training_dashboard()
